1baa3d7d90f3b8b2c2e34a3de9d0340c69943991
magnar wium <magnarw@gmail.com>
2013-10-01 23:29:47 +0200

    Added working code for step0.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,11 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('todoController', ['$scope',function($scope) {
+  	$scope.todos = []; 
+  	$scope.newTodo = ''; 
+  	$scope.addNewTodo = function () {
+  		$scope.todos.push({'message' : $scope.newTodo});
+  	};
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,12 +5,24 @@
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
+  describe('todoController', function() {
+  	var scope; 
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  	beforeEach(inject(function($rootScope, $controller) {
+  		scope = $rootScope.$new(); 
+  		var ctrl = $controller('todoController', {$scope:scope}); 
+  	})); 
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  	it('should not have any todos', function() {
+  		expect(scope.todos.length).toBe(0);
+  	});
+
+  	it('should have one todo after addTo has been called', function() {
+  		scope.newtodo = {'todo' : 'I have to do important stuff'}; 
+  		scope.addNewTodo(); 
+  		expect(scope.todos.length).toBe(1);
+  	});
+
+  });
+
 });

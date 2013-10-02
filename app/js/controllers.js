d842905171939f15bcaa951527eb8a01400bd098
Magnar Wium <magnarw@gmail.com>
2013-10-02 14:51:12 +0200

    Fixed tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,23 +11,43 @@ angular.module('myApp.controllers', []).
   }])
   .controller('todoController', ['$scope','$http',function($scope,$http) {
   	$scope.todos = [];
+    $scope.newTodo = '';
+
     $http.get('http://localhost:8080/ft-demo-pre/api/todoservice/').success(function(data) {
       $scope.todos = data;
     }).error(function(data) {
        $scope.todos = []; 
-    }); 
-  	$scope.newTodo = ''; 
-    $scope.id = 1; 
+    });
+
   	$scope.addNewTodo = function () {
-  		$scope.todos.push({'message' : $scope.newTodo, "done" : false, "id" : $scope.id});
-      $scope.id++; 
+  		var data = {'message' : $scope.newTodo, "done" : false};
+      $http({
+        url : 'http://localhost:8080/ft-demo-pre/api/todoservice/',
+        method: 'POST',
+        data: data,
+        headers: {'Content-Type':'application/json'}
+      }).success(function (data) {
+           $scope.todos.push(data);
+        }).error(function () {
+          alert('Noe gikk feil!');
+      });
   	};
+
     $scope.setDone = function (todo) {
       for(var x in $scope.todos){
         if($scope.todos[x].id === todo.id){
           $scope.todos[x].done = true; 
+          $http({
+              url : 'http://localhost:8080/ft-demo-pre/api/todoservice/' + $scope.todos[x].id,
+              method: 'PUT',
+              data: $scope.todos[x],
+              headers: {'Content-Type':'application/json'}
+          }).success(function (data) {
+            $scope.todos.push($scope.todos[x]);
+          }).error(function () {
+            alert('Noe gikk feil!');
+          });
         }
       }
-
     };
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,39 +6,47 @@ describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
   describe('todoController', function() {
-  	var scope; 
+  	var scope, httpMock; 
 
-  	beforeEach(inject(function($rootScope, $controller) {
-  		scope = $rootScope.$new(); 
+  	beforeEach(inject(function($rootScope, $controller,$httpBackend) {
+  		scope = $rootScope.$new();
+      httpMock = $httpBackend;
+      httpMock.when('GET', 'http://localhost:8080/ft-demo-pre/api/todoservice/').respond([{"message" : "have to do", "done" : false, "id" : 1}]);
   		var ctrl = $controller('todoController', {$scope:scope}); 
+      httpMock.flush();
   	})); 
 
   	it('should not have any todos', function() {
-  		expect(scope.todos.length).toBe(0);
-  	});
-
-  	it('should have one todo after addTo has been called', function() {
-  		scope.newtodo = {'todo' : 'I have to do important stuff'}; 
-  		scope.addNewTodo(); 
   		expect(scope.todos.length).toBe(1);
   	});
 
+  	it('should have one todo after addTo has been called', function() {
+      httpMock.expectPOST('http://localhost:8080/ft-demo-pre/api/todoservice/').respond({"message" : "I have to do important stuff", "done" : false, "id" : 2}); 
+  		scope.newtodo = {'message' : 'I have to do important stuff'}; 
+  		scope.addNewTodo(); 
+      httpMock.flush();
+  		expect(scope.todos.length).toBe(2);
+  	});
+
     it('should make sure that differnt todos has different ids', function() {
+      httpMock.expectPOST('http://localhost:8080/ft-demo-pre/api/todoservice/').respond({"message" : "I have to do important stuff", "done" : false, "id" : 3}); 
       scope.newtodo = {'todo' : 'I have to do important stuff'}; 
       scope.addNewTodo(); 
+      httpMock.flush();
+      httpMock.expectPOST('http://localhost:8080/ft-demo-pre/api/todoservice/').respond({"message" : "I have to do important stuff", "done" : false, "id" : 4}); 
       scope.newtodo = {'todo' : 'I have to some more important stuff'}; 
       scope.addNewTodo(); 
+      httpMock.flush();
       expect(scope.todos[0].id).not.toBe(scope.todos[1].id);
     });
-
+ 
     it('should make sure that todo is marked as done when done method is called', function() {
-      scope.newtodo = {'todo' : 'I have to do important stuff'}; 
-      scope.addNewTodo(); 
+      httpMock.expectPUT('http://localhost:8080/ft-demo-pre/api/todoservice/1').respond({}); 
       scope.setDone(scope.todos[0]); 
-      console.log(scope.todos[0]);
+      httpMock.flush();
       expect(scope.todos[0].done).toBe(true);
     });
-
+   
 
   });
 
